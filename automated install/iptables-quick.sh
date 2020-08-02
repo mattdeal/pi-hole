d6cac4863ce4a4b01cf70a19cb2ebba943cfd444
mattdeal <mwdeal1@gmail.com>
2020-08-01 20:16:25 -0400

    Update iptables-quick.sh

--- automated install/iptables-quick.sh
+++ automated install/iptables-quick.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 iptables -I INPUT 1 -s 192.168.0.0/16 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -I INPUT 1 -s 127.0.0.0/8 -p tcp -m tcp --dport 53 -j ACCEPT
 iptables -I INPUT 1 -s 127.0.0.0/8 -p udp -m udp --dport 53 -j ACCEPT
